93184dc01469279f27cea3f224efd3f85e31516b
ClubiNew <34558641+ClubiNew@users.noreply.github.com>
2022-04-09 14:54:37 -0400

    Updated publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     publicPath:
-        process.env.NODE_ENV === "production" ? "/wordle-forever/" : "/",
+        process.env.NODE_ENV === "production" ? "/Wordle-Forever/" : "/",
 };
